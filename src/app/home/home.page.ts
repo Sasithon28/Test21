338d2fd330942ac4651f8161ffdda91f6d4a4833
Sasithon28 <suchanatch2288@gmail.com>
2024-08-29 09:54:02 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public alertButtons = ['OK'];
+  public alertInputs = [
+    {
+      placeholder: 'Productname',
+    },
+    {
+      type: 'number',
+      placeholder: 'Price',
+      min: 1,
+      max: 100,
+    },
+    {
+      type: 'textarea',
+      placeholder: 'this is the product description',
+    },
+  ];
+
+  isActionSheetOpen = false;
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  setOpen(isOpen: boolean) {
+    this.isActionSheetOpen = isOpen;
+  }
 
   constructor() {}
 
